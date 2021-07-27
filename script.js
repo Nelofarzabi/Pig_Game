3094560a45bc7aa9d492c11a3e49595641e71f8a
Nilofar <Nelofarzabi2000@gmail.com>
2021-07-27 04:39:25 -0700

    update files

--- script.js
+++ script.js
@@ -3,13 +3,28 @@ var scors , activePlayer , dice , roundScor ;
 scors = [0 , 0];
 activePlayer = 1 ;
 roundScor = 0;
-dice = Math.floor(Math.random() * 6 )+1;
-console.log (dice);
-document.querySelector('#current--' + activePlayer ).textContent = dice;
+// three commint
+//
+// console.log (dice);
+// document.querySelector('#current--' + activePlayer ).textContent = dice;
  
-var scores = document.querySelector('#score--0').textContent;
-console.log (scores);
+// var scores = document.querySelector('#score--0').textContent;
+// console.log (scores);
+
+//four comment
 document.querySelector('.dice').style.display = 'none';
+document.getElementById('score--0').textContent = '0';
+document.getElementById('score--1').textContent = '0';
+document.getElementById('current--0').textContent = '0';
+document.getElementById('current--1').textContent = '0';
+
+document.querySelector('.btn--roll').addEventListener('click' , function(){
+   var dice = Math.floor(Math.random() * 6 )+1;
+    var domImg =document.querySelector('.dice');
+    domImg = style.display='block';
+    domImg = style.src= "dice-" + dice + '.png';
+
+})
 
 
  
