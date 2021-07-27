2c7aaf83cd61c1fd5cfdde5356dc718a464f050d
Nilofar <Nelofarzabi2000@gmail.com>
2021-07-27 04:58:54 -0700

    edit code

--- script.js
+++ script.js
@@ -13,16 +13,19 @@ roundScor = 0;
 
 //four comment
 document.querySelector('.dice').style.display = 'none';
+
 document.getElementById('score--0').textContent = '0';
 document.getElementById('score--1').textContent = '0';
 document.getElementById('current--0').textContent = '0';
 document.getElementById('current--1').textContent = '0';
 
 document.querySelector('.btn--roll').addEventListener('click' , function(){
-   var dice = Math.floor(Math.random() * 6 )+1;
-    var domImg =document.querySelector('.dice');
-    domImg = style.display='block';
-    domImg = style.src= "dice-" + dice + '.png';
+   var dice = Math.floor(Math.random() * 6 ) + 1;
+    var diceDom = document.querySelector('.dice');
+    diceDom.style.display='block';
+    diceDom.src='dice-' + dice + '.png';
+    
+  
 
 })
 
